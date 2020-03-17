a8f050fcc850f5b4f9719493c670d4969464023c
aliadame <33207912+aliadame@users.noreply.github.com>
2020-03-17 08:27:36 -0700

    Nueva versión de la clase estudiante
    En esta nueva versión implementamos un constructor personalizado diferente al que estaba por defecto

--- Practica3.py
+++ Practica3.py
@@ -1,9 +1,14 @@
 class Estudiante:
   # Indicamos atributos para la clase
-  edad = 0
-  carrera = "Desconocida"
+  edad = 0 #
+  carrera = "Desconocida" #
   universidad = "Desconocida"
-  genero = "Femenino"
+  genero = "Masculino"
+  
+  # Equivalente en java a public Estudiante(public Integer edad, public String carrera) {
+  def __init__(self, edad, carrera) :
+    self.edad = edad # paso de parametro en constructor
+    self.carrera = carrera
   
   # definimos funciones
   def festejar(self) :
@@ -21,11 +26,13 @@ class Estudiante:
   # Ajustamos la edad
   def cambiarEdad(self, edadAlumno) :
     self.edad = edadAlumno
-    print("Nueva edad ", edadAlumno)
+    print("Nueva edad " + str(edadAlumno))
     
 # Generamos un nuevo Estudiante
-miguel = Estudiante()
+# miguel = Estudiante()
+miguel = Estudiante(32, "lógica para programación")
+# Imprime: "Estudiando lógica para programación"
 miguel.estudiar("lógica para programación")
 # imprimimos atributo del objeto
-miguel.cambiarEdad(21)
+# miguel.cambiarEdad(21) # Imprime: "Nueva edad 21"
 print(miguel.edad)
